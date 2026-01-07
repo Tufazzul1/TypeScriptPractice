b0a7490d2689299d5a780b23d1b65e86d69ba7d8
Tufazzul1 <tufajjul2017@gmail.com>
2026-01-07 17:51:01 +0600

    3.5 DOne

---
+++ Module3/3.5.ts
@@ -0,0 +1,49 @@
+{
+
+    // Access Modifier in TypeScript
+
+    class BankAccount {
+        public readonly id: number;
+        public name: string;
+        protected _balance: number;
+
+        constructor(id: number, name: string, balance: number) {
+            this.id = id;
+            this.name = name;
+            this._balance = balance;
+        }
+
+        addDeposit(amount : number) {
+            this._balance = this._balance + amount;
+        }
+
+        getBalance(){
+            return this._balance;   
+        }
+
+    }
+
+    class studentAccount extends BankAccount {
+        test(){
+            this.
+        }
+
+    }
+
+    const goribsAccount = new BankAccount(111, "Gorib", 100);
+    goribsAccount.addDeposit(10);
+    console.log(goribsAccount.getBalance());
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
